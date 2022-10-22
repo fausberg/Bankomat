3fa21b125c6df528347157acaac5ecc7010cb56a
fausberg <ivanfadeev2003@mail.ru>
2022-10-22 20:07:29 +0300

    Bankomat2.0

--- src/Transaction.java
+++ src/Transaction.java
@@ -1,37 +1,28 @@
 import java.util.Scanner;
 
 public class Transaction {
-    public void transfer(CheckList checkList){
+    public void transfer(CheckList checkList) {
         CheckList checkList1 = new CheckList();
         boolean a = true;
         System.out.println("Введите номер карты которую хотите пополнить");
         String otherCard = new Scanner(System.in).nextLine();
         otherCard = examinationCardNumber(otherCard);
-        if(otherCard.length() == 16) {
-            checkList1 = new CheckList(otherCard);
-        }else{
-            System.out.println("Неправильно введен номер карты");
-            a = false;
-
-        }
         System.out.println("Введите сумму, которую хотите перевести");
         String sum = new Scanner(System.in).nextLine();
         sum = examinationSum(sum);
         int summ = Integer.parseInt(sum);
-        if(a) {
-            if (summ <= checkList.getBalance()) {
-                checkList1.setBalance(summ);
-                checkList.minusBalance(summ);
-            } else {
-                System.out.println("Недостаточно средств");
-            }
+        if (summ <= checkList.getBalance()) {
+            checkList1.setBalance(summ);
+            checkList.minusBalance(summ);
+        } else {
+            System.out.println("Недостаточно средств");
         }
     }
 
-    public String examinationCardNumber(String cardNumber){
-        cardNumber = cardNumber.replaceAll(" ","");
+    public String examinationCardNumber(String cardNumber) {
+        cardNumber = cardNumber.replaceAll(" ", "");
         String regex = "[0-9]+";
-        if(!cardNumber.matches(regex)){
+        if (!cardNumber.matches(regex) || cardNumber.length() != 16) {
             System.out.println("Неправильно введён номер карты. Введите его снова");
             cardNumber = new Scanner(System.in).nextLine();
             cardNumber = cardNumber.replaceAll(" ", "");
@@ -40,22 +31,22 @@ public class Transaction {
         return cardNumber;
     }
 
-    public void payment(CheckList checkList){
+    public void payment(CheckList checkList) {
         System.out.println("Введите сумму для оплаты");
         String sum = new Scanner(System.in).nextLine();
         sum = examinationSum(sum);
         int summ = Integer.parseInt(sum);
-        if(summ <= checkList.getBalance()) {
+        if (summ <= checkList.getBalance()) {
             checkList.minusBalance(summ);
-        }else{
+        } else {
             System.out.println("Недостаточно средств");
         }
     }
 
-    public String examinationSum(String sum){
-        sum = sum.replaceAll(" ","");
+    public String examinationSum(String sum) {
+        sum = sum.replaceAll(" ", "");
         String regex = "[0-9]+";
-        if(!sum.matches(regex)){
+        if (!sum.matches(regex)) {
             System.out.println("Неправильно введёна сумма. Введите её снова");
             sum = new Scanner(System.in).nextLine();
             examinationSum(sum);
